174685d4c10d040d9771daae029564a343492a34
Alberto Valenzuela <avalen28@gmail.com>
2022-11-20 20:54:26 +0100

    suffle cards done. 2 itertions lef

--- src/memory.js
+++ src/memory.js
@@ -1,11 +1,23 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards; // esto es la baraja (nombre + imagen)
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+    let shuffledArr = [];
+    let cloneCards = [...this.cards]; // correct duplicated
+    for (let i = 0; i < this.cards.length; i++) {
+      let randomIndex = Math.floor(Math.random() * cloneCards.length);
+      let randomCard = cloneCards.splice(randomIndex, 1);
+      shuffledArr.push(randomCard);
+    }
+    this.cards = shuffledArr;
   }
 
   checkIfPair(card1, card2) {
@@ -16,3 +28,4 @@ class MemoryGame {
     // ... write your code here
   }
 }
+console.log();
